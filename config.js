74e9b74e3fb108df266c14a479178b3b5690c11b
Takosuke <solidparallel6890@gmail.com>
2015-01-09 23:18:11 +0100

    set onair status to false

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {};
 
-config.onAir = true,
+config.onAir = false,
 config.port = 8000;
 config.programText = "";
 config.nextShow = "COMING UP: January 17, at 9pm Central European Time, LIVE FROM oTANNENBAUM";
