213b2cfca19b92ee09c91bdd47ef121b51f99652
Takosuke <solidparallel6890@gmail.com>
2016-04-28 10:45:08 +0200

    Moved everything down to html folder

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {};
-
-config.onAir = false,
-config.port = 8000;
-config.programText = "PIZZA SUICIDE TV LIVE FROM OTANNENBAUM, BERLIN - SATURDAY 17, 21PM CENTRAL EUROPEAN TIME";
-config.nextShow = "COMING UP: January 17, at 9pm Central European Time, LIVE FROM oTANNENBAUM";
-
-module.exports = config;
