b96c85976ce7ae1123958574ee33a0f7cef3c097
Takosuke <solidparallel6890@gmail.com>
2016-05-12 14:10:16 +0200

    Deploy scripts WIP

---
+++ devops/roles/update_information/templates/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.onAir = true
+config.port = 8000;
+config.programText = "THE HANgoVER SHOW COMING UP!";
+config.nextShow = "FIRST SHOW OF THE YEAR";
+config.countDownTo = "2016-01-15T21:00:00+0000"
+
+module.exports = config;
---
+++ html/config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.onAir = false
+config.port = 8000;
+config.programText = "The Pizza Suicide TV Show";
+config.nextShow = "Coming up";
+config.countDownTo = "2028-01-15T21:00:00+0000"
+
+module.exports = config;
